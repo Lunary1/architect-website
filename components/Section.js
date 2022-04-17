087ef8d17d4b4ffda4d94281ddb6834282cacd98
Lunary1 <wouters.felix@gmail.com>
2022-04-17 02:00:05 +0200

    changes

--- components/Section.js
+++ components/Section.js
@@ -7,10 +7,10 @@ import Image from 'next/image';
 export default function Section() {
     return (
 
-        <div className="container mx-auto mt-20 md:px-4">
-            <div className="flex justify-between">
+        <div className="container px-4 md:mx-auto mt-20">
+            <div className="md:flex justify-between">
                 <div className="left">
-                    <h2 className="text-white bg-black uppercase px-1">Chapter NO. 01</h2>
+                    <h2 className="text-white bg-black uppercase px-1 h-0 md:h-auto">Chapter NO. 01</h2>
                 </div>
                 <div className="right font-serif italic border-t-2 pt-2 text-xl">
                     <p>In 1939, Holger Nielsen wrote the first chapter of the Vipp <br/> story when he crafted a pedal bin for his wife&apos;s hairdressing <br/> salon.</p>
@@ -36,11 +36,11 @@ export default function Section() {
             </div>
 
             <div className="text-center">
-                <h1 className="font-serif text-5xl my-16">Media</h1>
+                <h1 className="font-serif text-5xl my-8 md:my-16">Media</h1>
             </div>
 
-            <div className="grid grid-cols-3 gap-2 my-3">
-                <div className="carousel">
+            <div className="grid grid-cols-1 gap-2 my-8 md:grid-cols-3">
+                <div className="carousel h-0 md:h-auto">
                     <div className="carousel-item w-full">
                         <img src="https://api.lorem.space/image/game?w=256&h=400&hash=500B67FB" className="w-full" alt="Tailwind CSS Carousel component" />
                     </div> 

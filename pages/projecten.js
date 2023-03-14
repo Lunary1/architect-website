12c97f158eb06170d7139e3fdfdf76a29d273438
Lunary1 <wouters.felix@gmail.com>
2023-03-14 17:51:49 +0100

    server changed

--- pages/projecten.js
+++ pages/projecten.js
@@ -40,7 +40,9 @@ function Projecten({ data }) {
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(`http://localhost:3000/api/projects/getProjects-lib`);
+  const res = await fetch(
+    `https://architect-website-five.vercel.app/api/projects/getProjects-lib`
+  );
   const data = await res.json();
 
   // Pass data to the page via props

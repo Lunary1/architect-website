dc1f31431a5df71f8d8639f33048ed30f4d7bc91
Lunary1 <wouters.felix@gmail.com>
2023-03-30 01:11:02 +0200

    changes

--- pages/api/projects/getProjectById-lib.js
+++ pages/api/projects/getProjectById-lib.js
@@ -1,6 +1,7 @@
 import { query } from "../../../lib/db";
 
 export default async function handler(req, res) {
+  const id = req.body.id
   try {
     const querySql =
       "SELECT projectId, projectName, location FROM projects WHERE projectId = ?";
---
+++ pages/projecten/[id].js
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function projectOverview() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [dataResponse, setDataResponse] = useState([]);
+
+  useEffect(() => {
+    async function getPageData() {
+      const apiUrlEndpoint = `https://architect-website-five.vercel.app/api/projects/getProjectById-lib`;
+      const postData = {
+        method: "Post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            id: id,
+        }),
+      };
+
+      const response = await fetch(apiUrlEndpoint);
+      const res = await response.json();
+      setDataResponse(res.projects);
+    }
+    getPageData();
+  }, [router.query.id, router.isReady]);
+
+  return(
+
+    {id}
+  )
+}

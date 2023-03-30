01a9aaf10c5fe609b124d9b9e828e6e03e084de6
Lunary1 <wouters.felix@gmail.com>
2023-03-30 18:31:34 +0200

    test

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,7 @@
-import paul from "../public/paul-about.jpg"
-import Image from 'next/image'
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 function About() {
-
   const [scrollPosition, setScrollPosition] = useState(0);
   const handleScroll = () => {
     const position = window.pageYOffset;
@@ -23,21 +21,31 @@ function About() {
       <div className="grid py-36 gap-4 md:grid-cols-2 sm:grid-cols-1">
         <div className="left py-20 text-white">
           <div className="header-title mb-24">
-            <progress className="progress progress-info w-56" value={scrollPosition} max="1000"></progress>
+            <progress
+              className="progress progress-info w-56"
+              value={scrollPosition}
+              max="1000"
+            ></progress>
             <h1 className="text-5xl">Over Architect Kindt</h1>
             <h2 className="text-l mt-4">The story behind</h2>
           </div>
-          <p className="text-md leading-relaxed whitespace-pre-wrap">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat mollitia consectetur animi dolor velit eius vitae eum exercitationem dolorum? Dolore labore laudantium ea earum aut quis sit vitae voluptates architecto.</p>
-          <p className="text-md leading-relaxed whitespace-pre-wrap mt-4">Deleniti, repudiandae?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, ea, alias quas expedita dolorum dolore molestias reprehenderit sint eius laboriosam laudantium aut quaerat mollitia deserunt quam, libero odio similique sequi?</p>
-
-
+          <p className="text-md leading-relaxed whitespace-pre-wrap">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            mollitia consectetur animi dolor velit eius vitae eum exercitationem
+            dolorum? Dolore labore laudantium ea earum aut quis sit vitae
+            voluptates architecto.
+          </p>
+          <p className="text-md leading-relaxed whitespace-pre-wrap mt-4">
+            Deleniti, repudiandae?Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Pariatur, ea, alias quas expedita dolorum dolore
+            molestias reprehenderit sint eius laboriosam laudantium aut quaerat
+            mollitia deserunt quam, libero odio similique sequi?
+          </p>
         </div>
-        <div className="right">
-          <Image src={paul} width={800} height={800} alt="Foto Paul" />
-        </div>
+        <div className="right"></div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;

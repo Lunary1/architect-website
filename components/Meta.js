3920bf0c059b8a84ad6fe055260b6812e412f5f6
Lunary1 <wouters.felix@gmail.com>
2023-04-01 04:07:15 +0200

    mistake 2

--- components/Meta.js
+++ components/Meta.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-const Meta = ({ keywords, description }) => {
+const Meta = ({ keywords, description, title }) => {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -8,34 +8,6 @@ const Meta = ({ keywords, description }) => {
       <meta name="description" content={description} />
       <meta charSet="utf-8" />
       <title>Architect Paul Kindt</title>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="true"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 };

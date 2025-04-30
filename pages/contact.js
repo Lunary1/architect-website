30477493da2fd23a60a57e40b9c980378bc68e25
Felix Wouters <felix.wouters@brightest.be>
2025-04-30 17:02:41 +0200

    vercel changes 3.0

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Hero from "../components/Hero";
 import ContactForm from "../components/ContactForm";
-import Map from "../components/Map";
 
 const contact = () => {
   return (
